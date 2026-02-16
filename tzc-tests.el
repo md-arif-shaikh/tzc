42245577e3f3056f9241d8071b9743aec83911cc
md-arif-shaikh <arifshaikh.astro@gmail.com>
2026-02-16 20:54:53 +0530

    fix test

--- tzc-tests.el
+++ tzc-tests.el
@@ -1,5 +1,6 @@
 (require 'ert)
 (load-file "tzc.el")
+(load-file "tzc-org.el")
 
 (ert-deftest tzc-test-get-offset-lisbon ()
   (should (equal (mapcar (lambda (date) (tzc--get-offset "Europe/Lisbon" date)) (list "2024-03-25" "2024-04-05")) (list "+0000" "+0100"))))
